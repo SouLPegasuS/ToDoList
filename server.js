39ffa1918b550b1a59d39a5c689b1fa727b7fb84
SouLPegasuS <pratham7910@gmail.com>
2021-07-06 23:04:33 +0530

    served html file

--- server.js
+++ server.js
@@ -25,19 +25,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 passport.use(User.createStrategy());
 
-app.use(cors({
-  origin: "https://todo-list-reactapp.herokuapp.com/"
-}));
+// app.use(cors({
+  // origin: "https://todo-list-reactapp.herokuapp.com/"
+// }));
 
 app.use("/users", userRoutes);
 app.use("/todo", todoRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  // const path = require("path");
-  // app.get("*", function(req, res) {
-  //     res.sendFile(path.resolve(__dirname,"client","build","index.html"));  
-  // });
+  const path = require("path");
+  app.get("*", function(req, res) {
+      res.sendFile(path.resolve(__dirname,"client","build","index.html"));  
+  });
 }
 
 app.listen(PORT, () => {
